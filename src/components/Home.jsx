55332666a941b88f63b34f098a013e663f641614
Duba Halakhe <dubahdida@gmail.com>
2025-10-26 21:01:04 +0300

    home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to Pet Care Tracker</h1>
+      <p>Track your pets grooming, exercise, and health easily </p>
+    </div>
+  );
+}
+
+export default Home;
