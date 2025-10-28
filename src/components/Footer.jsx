bbecf3517cda92c31c1b700b194ac2e6c192a2bc
Elly Omondi <owuorelly3@gmail.com>
2025-10-28 18:34:47 +0300

    Homepage Additions2

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      <p>🐾 Happy Pets, Happier Owners — Pet Care Tracker © 2025</p>
-    </footer>
-  );
-}
-
-export default Footer;
